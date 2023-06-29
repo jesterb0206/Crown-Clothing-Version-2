cfb3a16d80dbc3e7051d841286ee6c9a20a1e622
jesterb0206 <jesterb@seattleu.edu>
2023-06-28 18:38:03 -0700

    Categories Query

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -1,21 +1,28 @@
 import {createContext, useState, useEffect} from 'react';
-import {getCategoriesAndDocuments} from '../utils/firebase/firebase.utils';
+import {gql, useQuery} from '@apollo/client';
 
 export const CategoriesContext = createContext({
   categoriesMap: {},
 });
 
+const COLLECTIONS = gql`
+  query {
+    collections {
+      id
+      title
+      items {
+        id
+        name
+        price
+        imageUrl
+      }
+    }
+  }
+`;
+
 export const CategoriesProvider = ({children}) => {
+  const {loading, error, data} = useQuery(COLLECTIONS);
   const [categoriesMap, setCategoriesMap] = useState({});
-
-  useEffect(() => {
-    const getCategoriesMap = async () => {
-      const categoryMap = await getCategoriesAndDocuments();
-      setCategoriesMap(categoryMap);
-    };
-
-    getCategoriesMap();
-  }, []);
 
   const value = {categoriesMap};
 
